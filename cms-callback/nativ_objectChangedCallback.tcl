aa2b6e35dbca67f7c41cfd804e84d00760118633
Bartek Ochab <bartek.ochab@infopark.de>
2017-06-02 09:57:12 +0200

    provided alternativ and faster object chnaged callback

---
+++ cms-callback/nativ_objectChangedCallback.tcl
@@ -0,0 +1,17 @@
+proc objectChangedCallback {messages} {
+  add_to_redis $messages "index_[app get instanceName]" "Infopark::SES::Indexer"
+}
+
+proc add_to_redis { messages QUEUE CLASS } {
+  set NAMESPACE "resque"
+  append protocoll "SADD ${NAMESPACE}:queues ${QUEUE}\r\n"
+  foreach m $messages {
+    append protocoll "RPUSH ${NAMESPACE}:queue:${QUEUE} \"{\\\"class\\\":\\\"${CLASS}\\\",\\\"args\\\":\\\"${m}\\\"}\"\r\n"
+  }
+
+  logMessage 1 "sending to redis:\n $protocoll"
+  set redis [ open "|redis-cli --pipe" w ]
+  puts -nonewline $redis $protocoll
+  close $redis
+}
+
